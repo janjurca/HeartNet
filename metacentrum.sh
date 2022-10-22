908c1baaed08254120d78a5cb684624e9c856d41
Jan Jurca <jjurca@redhat.com>
2022-10-22 20:59:48 +0200

    fix

--- metacentrum.sh
+++ metacentrum.sh
@@ -1,23 +1,19 @@
 
 # METACENTRUM
 # qsub -I -q gpu -l select=1:ncpus=2:ngpus=1:mem=16gb:scratch_local=30GB:gpu_cap=cuda80 -l walltime=24:00:00
-    
+
 module add cuda-9.0
 module load cudnn-7.0
+module add python36-modules-gcc
 
 cd $SCRATCHDIR || exit 1
 mkdir temp
 TMPDIR=$(pwd)/temp
 export TMPDIR
-wget https://bootstrap.pypa.io/get-pip.py
-python3.7 get-pip.py
-python3.7 -m pip install numpy  --cache-dir $(pwd)/cache --root $(pwd) -I
-python3.7 -m pip install torch torchvision --cache-dir $(pwd)/cache --root $(pwd) -I
-export PYTHONPATH=$(pwd)/usr/local/lib/python3.7/dist-packages/:$PYTHONPATH
+pip3.6 install torch torchvision SimpleITK simpy scipy --cache-dir $(pwd)/cache --root $(pwd) -I
+export PYTHONPATH=$(pwd)/software/python-3.6.2/gcc/lib/python3.6/site-packages/:$PYTHONPATH
 
-
-cd /storage/brno2/home/xjurca08/storage/brno2/home/xjurca08
-git clone https://github.com/janjurca/HeartNet.git
-cd HeartNet
+cd /storage/brno2/home/xjurca08/storage/brno2/home/xjurca08/HeartNet
 git pull
 
+python3.6 train.py --dataset /storage/brno2/home/xjurca08/storage/brno2/home/xjurca08/Gomez_T1/ --batchSz 4
